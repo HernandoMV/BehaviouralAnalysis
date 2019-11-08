af912d1f1c57092dc7161923283544721024d5b8
HernandoMV <hernybiotec@gmail.com>
2019-11-08 09:48:01 +0000

    lots of changes

--- utils/custom_functions.py
+++ utils/custom_functions.py
@@ -3,7 +3,7 @@
 import numpy as np
 import re
 import ntpath
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV, LinearRegression
 import pandas as pd
 import datetime
 from itertools import chain, compress
@@ -55,7 +55,7 @@ def BpodDatesToTime(inputDates):
     return(outputDates)
 
 
-def PsychPerformance(trialsDif, sideSelected):  
+def PsychPerformance(trialsDif, sideSelected):
     # function to calculate psychometric performance and fit logistic regression to the data
     # returns a dictionary
 
@@ -65,11 +65,11 @@ def PsychPerformance(trialsDif, sideSelected):
         nan_mask = ~(np.isnan(trialsDif) | np.isnan(sideSelected))
         # logistic regression
         try:
-            clf = LogisticRegression().fit(trialsDif[nan_mask, np.newaxis], sideSelected[nan_mask])
+            clf = LogisticRegressionCV().fit(trialsDif[nan_mask, np.newaxis], sideSelected[nan_mask])
         except Exception:
             # in case a model cannot be fitted (e.g. mouse always goes to the left)
             # fit model on dummy data
-            clf = LogisticRegression().fit(np.array([0, 0, 100, 100]).reshape(-1, 1), np.array([1, 0, 1, 0]))
+            clf = LogisticRegressionCV().fit(np.array([0, 0, 100, 100]).reshape(-1, 1), np.array([1, 0, 1, 0]))
         # Calculate performance
         # Initialize values
         difficulty = np.unique(trialsDif[~np.isnan(trialsDif)])
@@ -86,41 +86,41 @@ def PsychPerformance(trialsDif, sideSelected):
             'Logit': clf}
     else:
         DictToReturn = {}
-        
+
     return DictToReturn
 
 
 def splitOpto(SessionData):
     # SessionData comes from bpod: ExperimentData[x]['SessionData']
     # Returns two dictionaries
-    
+
     Trials_normalMask = SessionData['OptoStim'] == 0
     Trials_optoMask = SessionData['OptoStim'] == 1
-    
+
     # selection of normal and opto trials
     normalTrials_sideSelected = SessionData['FirstPoke'][Trials_normalMask]
     normalTrials_difficulty = SessionData['TrialHighPerc'][Trials_normalMask]
     optoTrials_sideSelected = SessionData['FirstPoke'][Trials_optoMask]
     optolTrials_difficulty = SessionData['TrialHighPerc'][Trials_optoMask]
-    
+
     # create dictionaries
     NormalTrials = {
         'SideSelected': normalTrials_sideSelected,
         'Difficulty': normalTrials_difficulty
     }
-    
+
     OptoTrials = {
         'SideSelected': optoTrials_sideSelected,
         'Difficulty': optolTrials_difficulty
     }
-    
+
     return NormalTrials, OptoTrials
 
 
 def generate_fake_data(trialsDif, sideSel):
     # Generates data for bootstrapping, sampling and replacing, so each
     # unique trialsDif maintains the same size
-    
+
     fake_side_sel = np.empty_like(sideSel)
     for curr_diff in np.unique(trialsDif):
         diff_mask = trialsDif == curr_diff
@@ -142,14 +142,14 @@ def BootstrapPerformances(trialsDif, sideSelected, ntimes, prediction_difficulti
         # create fake data
         fake_data = generate_fake_data(difficulties, sideselection)
         try:
-            clf_fake = LogisticRegression().fit(difficulties.reshape(-1, 1), fake_data)
-        except:
+            clf_fake = LogisticRegressionCV().fit(difficulties.reshape(-1, 1), fake_data)
+        except Exception:
             # in case a model cannot be fitted (e.g. mouse always goes to the left)
             # fit model on dummy data
-            clf_fake = LogisticRegression().fit(np.array([0,0,100,100]).reshape(-1, 1), np.array([1,0,1,0]))
-            
+            clf_fake = LogisticRegressionCV().fit(np.array([0, 0, 100, 100]).reshape(-1, 1), np.array([1, 0, 1, 0]))
+
         predictPerFake[:, i] = 100 * clf_fake.predict_proba(prediction_difficulties)[:, 1]
-        
+
     return predictPerFake
 
 
@@ -161,35 +161,35 @@ def SessionDataToDataFrame(AnimalID, ExperimentalGroup, SessionID, SessionData):
     if not 'nTrials' in SessionData:
         print('Session is empty')
         return pd.DataFrame()
-    
+
     numberOfTrials = SessionData['nTrials']
-    
+
     # protocol information
     ts = SessionData['TrialSettings']
     protocols = [ts[0]['GUIMeta']['TrainingLevel']['String'][x] for x in [y['GUI']['TrainingLevel'] - 1 for y in ts]]
     stimulations = [ts[0]['GUIMeta']['OptoStim']['String'][x] for x in [y['GUI']['OptoStim'] - 1 for y in ts]]
-    
+
     # muscimol
     yList = []
     for y in ts:
         try:
             yList.append(y['GUI']['Muscimol'] - 1)
-        except:
+        except Exception:
             yList.append(0)
     muscimol = []
     for x in yList:
         try:
             muscimol.append(ts[0]['GUIMeta']['Muscimol']['String'][x])
-        except:
+        except Exception:
             muscimol.append('No')
-    
+
     if not np.logical_and(len(protocols) == numberOfTrials, len(stimulations) == numberOfTrials):
         print('protocols and/or stimulations length do not match with the number of trials')
         return pd.DataFrame()
     CenterPortDuration = [x['GUI']['CenterPortDuration'] for x in ts]
     Contingency = [x['GUI']['Contingency'] for x in ts]
     RewardAmount = [x['GUI']['RewardAmount'] for x in ts]
-    
+
     # trial events
     trev = [x['Events'] for x in SessionData['RawEvents']['Trial']]
     if not len(trev)==numberOfTrials:
@@ -265,12 +265,12 @@ def AnalyzeSwitchTrials(df):
     # 'SessionTime'
     # 'OptoStim'
     # it returns a different dataframe with information grouped for a bar plot
-    
+
     # get info for the sessions
     sessionsID = pd.unique(df['SessionTime'])
     # initialize list to hold dataframes
     sessionsInfo = []    
-    
+
     # fill the new dataframe with info for each session
     for session in sessionsID:
         # get the dataframe for that session
@@ -293,7 +293,7 @@ def AnalyzeSwitchTrials(df):
                                  })
         # append it to list
         sessionsInfo.append(SessionDF)
-        
+
     # merge into a single df and return
     return pd.concat(sessionsInfo, ignore_index=True)
 
@@ -303,7 +303,7 @@ def PP_ProcessExperiment(SessionData, bootstrap=None, error_bars=None):
     # SessionData is a dataframe that needs to have the following column names:
     # 'TrialHighPerc'
     # 'FirstPoke'
-    
+
     diffs = np.array(SessionData['TrialHighPerc'])
     choices = np.array(SessionData['FirstPoke'])
 
@@ -313,7 +313,7 @@ def PP_ProcessExperiment(SessionData, bootstrap=None, error_bars=None):
     predictDif = np.linspace(1, 100, 2000).reshape(-1, 1)
     if PsyPer:
         predictPer = 100 * PsyPer['Logit'].predict_proba(predictDif)[:,1]
-        
+
         # Calculate the error bars if asked to
         if error_bars is not None:
             EBdata = SessionData.groupby(by=error_bars).apply(getEBdata)
@@ -324,19 +324,19 @@ def PP_ProcessExperiment(SessionData, bootstrap=None, error_bars=None):
             Std_list = [np.std(list(compress(EB_perfs_flat, EB_diffs_flat == dif))) for dif in PsyPer['Difficulty']]
         else:
             Std_list = np.nan
-    else: # needed for the return
+    else:  # needed for the return
         predictPer = np.nan
         Std_list = np.nan
-        
+
     # Bootstrap on fake data (generated inside the bootstrap function)
     fakePredictions = np.nan
     if bootstrap is not None:
         np.random.seed(12233)  # fixed random seed for reproducibility
         if PsyPer:
-            fakePredictions = BootstrapPerformances(trialsDif = diffs,
-                                                    sideSelected = choices,
-                                                    ntimes = bootstrap,
-                                                    prediction_difficulties = predictDif)          
+            fakePredictions = BootstrapPerformances(trialsDif=diffs,
+                                                    sideSelected=choices,
+                                                    ntimes=bootstrap,
+                                                    prediction_difficulties=predictDif)
 
     # return what is needed for the plot
     return predictDif, PsyPer, fakePredictions, predictPer, Std_list
@@ -346,12 +346,12 @@ def getEBdata(SessionData):
     # SessionData is a dataframe that needs to have the following column names:
     # 'TrialHighPerc'
     # 'FirstPoke'
-    
+
     diffs = np.array(SessionData['TrialHighPerc'])
     choices = np.array(SessionData['FirstPoke'])
-    
+
     PsyPer = PsychPerformance(trialsDif=diffs, sideSelected=choices)
-    
+
     return PsyPer
 
 
@@ -384,37 +384,37 @@ def RBias(FirstPokes, FirstPokesCorrect):
     % FirstPokes is a vector of 1s and 2s (Left or Right), indicating the poked port
     % FirstPokesCorrect is a 0 and 1 vector (wrong or correct poke)
     % Both could have NaN values
-    
+
     % Returns from -1 to 1. 0 Being not biased, 1 being Right-biased, and
     % -1 being left-biased. It is a conservative function. E.g, in a 50-50
     % trial chance, and being totally biased to one side, only half of the
     % trials would be wrong, so the function would output +/-0.5.
-    
+
     % Correct trials based on proportion of wrong pokes
     % Determine the proportion of wrong pokes to the right side
     '''
     WrongSides = FirstPokes[FirstPokesCorrect == 0]
-    if len(WrongSides)<1:
+    if len(WrongSides) < 1:
         RBias = 0
     else:
-        WrongSideProportion = len(WrongSides) / len(FirstPokes) # from 0 to 1
-        WrongRightsProportion = WrongSideProportion * np.nansum(WrongSides == 2)/len(WrongSides)
-        WrongLeftsProportion = WrongSideProportion * np.nansum(WrongSides == 1)/len(WrongSides)
+        WrongSideProportion = len(WrongSides) / len(FirstPokes)  # from 0 to 1
+        WrongRightsProportion = WrongSideProportion * np.nansum(WrongSides == 2) / len(WrongSides)
+        WrongLeftsProportion = WrongSideProportion * np.nansum(WrongSides == 1) / len(WrongSides)
 
         RBias = WrongRightsProportion - WrongLeftsProportion
     return RBias
 
 
 def CalculateRBiasWindow(FirstPokes, FirstPokesCorrect, Window):
-    '''Calculates RBias over the lenght of the vectors FirstPokes and 
+    '''Calculates RBias over the lenght of the vectors FirstPokes and
     FirstPokesCorrect using a Window. Returns vector of same lenght'''
     # Create empty vector
     RBiasVector = np.empty(len(FirstPokes))
     RBiasVector[:] = np.nan
     for i in range(Window, len(FirstPokes)):
-        win = range((i-Window),i)
+        win = range((i - Window), i)
         RBiasVector[i] = RBias(FirstPokes[win], FirstPokesCorrect[win])
-    
+
     return RBiasVector
 
 
@@ -426,17 +426,29 @@ def CalculateMidPokes(df):
 
 # quantify how long they wait in the middle
 def MidPortWait(df):
-    timeOut = df['TrialStates']['WaitForResponse'].astype('float32')[0]  
+    timeOut = df['TrialStates']['WaitForResponse'].astype('float32')[0]
     PortIn = df['TrialEvents']['Port2In']
+    # sometimes this is an integer (rarely)
+    if isinstance(PortIn, int):
+        PortIn = float(PortIn)
     if not isinstance(PortIn, float):
-        PortIn = PortIn.astype('float32')
-        PortInIdx = np.where(PortIn<timeOut)[0][-1]
+        PortIn = PortIn.astype('float32')  # does not work for int
+        PortInIdx = np.where(PortIn < timeOut)[0][-1]
         PortInTime = PortIn[PortInIdx]
     else:
         PortInTime = PortIn
-    
+
     PortTime = timeOut - PortInTime
     return PortTime
+
+
+# quantify the time they take to initiate a trial (from trialstart to center poke in)
+def CalculateTrialInitiationTime(df):
+    # the first time they poke
+    try:
+        return float(df.TrialEvents['Port2In'][0])
+    except Exception:
+        return float('NaN')
 
 
 def AnalyzePercentageByDay(rdf):
@@ -445,59 +457,67 @@ def AnalyzePercentageByDay(rdf):
     # 'TrainingDay'
     # 'AnimalID'
     # 'Protocol'
-    # 'Injection'
+    # 'ExperimentalGroup'
     # it returns a different dataframe with information grouped for a bar plot
     AnimalIDs = pd.unique(rdf['AnimalID'])
     animalsInfo = []
     for animalid in AnimalIDs:
-        df = rdf[rdf['AnimalID']==animalid]
+        df = rdf[rdf['AnimalID'] == animalid]
         # get info for the sessions
         TrainingDays = pd.unique(df['TrainingDay'])
+        # initialize value for cumulative trials
+        CumTrials = 0
         # fill the new dataframe with info for each session
         for session in TrainingDays:
             # get the dataframe for that session
-            Sdf = df[df['TrainingDay']==session]
-            # protocol and injection
+            Sdf = df[df['TrainingDay'] == session]
+            # protocol and ExperimentalGroup
             prot = Sdf.Protocol.iloc[0]
-            inj = Sdf.Injection.iloc[0]
+            inj = Sdf.ExperimentalGroup.iloc[0]
             # percentage of correct trials
-            PercCorrect = 100 * np.sum(Sdf['FirstPokeCorrect'])/len(Sdf)
+            PercCorrect = 100 * np.sum(Sdf['FirstPokeCorrect']) / len(Sdf)
+            # number of trials per session
+            NumOfTrials = len(Sdf)
+            # cumulative trials
+            CumTrials = CumTrials + NumOfTrials
             # fill the dataframe
             SessionDF = pd.DataFrame({
                                       'AnimalID': animalid,
                                       'SessionTime': session,
                                       'PercCorrect': np.array([PercCorrect]),
+                                      'NumberOfTrials': NumOfTrials,
+                                      'CumulativeTrials': CumTrials,
                                       'Protocol': prot,
-                                      'Injection': inj
+                                      'ExperimentalGroup': inj
                                      })
             # append it to list
             animalsInfo.append(SessionDF)
     # merge into a single df and return
-    
+
     return pd.concat(animalsInfo, ignore_index=True)
 
 
-def ReadAnimalData(filelist, printout = True):
+def ReadAnimalData(filelist, printout=True):
     # Reads all data from one animal and one protocol
-    
+
     # Initialize return lists
-    ExperimentFiles = [] # to store experiment names
-    ExperimentData = [] # to store the dictionaries
-    ntrialsDistribution = [] # to visualize the distribution of the number of trials
-    Protocols = [] # store the protocols
-    Stimulations = [] # store the stimulated protocols
-    Muscimol = [] # store information about the muscimol
+    ExperimentFiles = []  # to store experiment names
+    ExperimentData = []  # to store the dictionaries
+    ntrialsDistribution = []  # to visualize the distribution of the number of trials
+    Protocols = []  # store the protocols
+    Stimulations = []  # store the stimulated protocols
+    Muscimol = []  # store information about the muscimol
     counter = 0
     # sort the file list
-    filelist.sort()
+    filelist = sorted(filelist, key=str.casefold)
     for file in filelist:
         # Read data
         data = load_ns.loadmat(file)
-        
+
         # if that session is empty skip it:
         if not 'nTrials' in data['SessionData']:
             continue
-        
+
         ntrials = data['SessionData']['nTrials'] # this sometimes fails if the session is empty
 
         ExperimentFiles.append(file)
@@ -507,32 +527,32 @@ def ReadAnimalData(filelist, printout = True):
             for trial_num, trial in enumerate(trial_settings):
                 trial_settings[trial_num] = load_ns._todict(trial)
             data['SessionData']['TrialSettings'] = trial_settings
-        except:
+        except Exception:
             data['SessionData']['TrialSettings'] = np.nan
 
         # Get info for the settings from the first trial
         try:
             protocol = trial_settings[0]['GUIMeta']['TrainingLevel']['String'][
                 trial_settings[0]['GUI']['TrainingLevel'] - 1]
-        except:
+        except Exception:
             protocol = 'Unknown'
 
         try:
             stimulation = trial_settings[0]['GUIMeta']['OptoStim']['String'][
                 trial_settings[0]['GUI']['OptoStim'] - 1]
-        except:
+        except Exception:
             stimulation = 'unknown'
-        
+
         try:
             muscimol = trial_settings[0]['GUIMeta']['Muscimol']['String'][
                 trial_settings[0]['GUI']['Muscimol'] - 1]
-        except:
+        except Exception:
             muscimol = 'unknown'
-        
+
         if printout:
             print('{}: {}, {} trials on {}, stim {}, muscimol {}'.format(\
                     counter, ntpath.basename(file), ntrials, protocol, stimulation, muscimol))
-        
+
         ntrialsDistribution.append(ntrials)
         Protocols.append(protocol)
         Stimulations.append(stimulation)
@@ -545,14 +565,13 @@ def ReadAnimalData(filelist, printout = True):
             for trial_num, trial in enumerate(trial_raw_events):
                 trial_raw_events[trial_num] = load_ns._todict(trial)
             data['SessionData']['RawEvents']['Trial'] = trial_raw_events
-        except:
+        except Exception:
             data['SessionData']['RawEvents']['Trial'] = np.nan
-        
 
         # Save the data in a list
         ExperimentData.append(data)
-        counter+=1
-        
+        counter += 1
+
     return ExperimentFiles, ExperimentData, ntrialsDistribution, Protocols, Stimulations, Muscimol
 
 
@@ -594,4 +613,47 @@ def split_files_into_old_and_new(filelist, existing_dates):
     new_files = [dif_files[i] for i in list(set(range(len(dif_files))) - set(old_idx))]
 
     return old_files, new_files
+
+
+def perf_window_calculator(df, window):
+    """
+    Calculate the performance of the last X trials
+
+    """
+    firstpokecorrect = df['FirstPokeCorrect']  # 0s and 1s
+    # create empty vector of the same size
+    perf_window = np.full(len(firstpokecorrect), np.nan)
+    for i in range(window - 1, len(perf_window)):
+        perf_window[i] = np.sum(firstpokecorrect[i - window + 1: i + 1]) / window * 100
+    return perf_window
+
+
+# calculate the trials per minute that animals do by fitting a line
+def trials_per_minute(trial_index, trial_start_timestamp):
+    """
+    function to calculate the speed of the mouse in trials per minute
+    param trial_index: pandas.core.series.Series with the trial index
+    param trial_start_timestamp: pandas.core.series.Series with the trial start time in seconds
+    returns a value which is the trials per minute
+    """
+    lrmodel = LinearRegression().fit(trial_index[:, np.newaxis], trial_start_timestamp)
+
+    return 60 * 1 / lrmodel.coef_[0]
+
+
+def speed_window_calculator(df, window):
+    """
+    Calculate the speed over X trials
+
+    """
+    trial_index = df.TrialIndex
+    trial_start_timestamp = df.TrialStartTimestamp
+    # create empty vector of the same size
+    speed_window = np.full(len(trial_index), np.nan)
+    for i in range(int(window / 2) - 1, len(speed_window) - int(window / 2)):
+        win_idx_low = i - int(window / 2) + 1
+        win_idx_high = i + int(window / 2)
+        speed_window[i] = trials_per_minute(trial_index[win_idx_low: win_idx_high],
+                                            trial_start_timestamp[win_idx_low: win_idx_high])
+    return speed_window
 
